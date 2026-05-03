3ed7c2d6d3eac948555ab4d5c6b547ef9fff66ed
Michael Lamothe <michael.lamothe@gmail.com>
2026-05-03 10:13:30 +1000

    Added command line options.

--- Program.cs
+++ Program.cs
@@ -2,12 +2,12 @@
 using Tmds.DBus.Protocol;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Gtk;
 
 const int braviaPort = 20060;
 const string braviaPowerOffCommand = "*SCPOWR0000000000000000";
 const string braviaPowerOnCommand = "*SCPOWR0000000000000001";
 const string braviaSetInputHdmi1Command = "*SCINPT0000000100000001";
-
 const string applicationName = "gnome-bravia-screensaver";
 
 using var factory = LoggerFactory.Create(builder => builder.AddSystemdConsole());
@@ -25,7 +25,7 @@ var config = new ConfigurationBuilder()
     .Build();
 
 // Get values from the config given their key and their target type.
-var settings = config.GetRequiredSection("Bravia").Get<BraviaSettings>()
+var settings = config.GetRequiredSection(nameof(BraviaSettings)).Get<BraviaSettings>()
     ?? throw new Exception("Failed to load settings.");
 
 logger.LogInformation("Bravia TV IP address is {IPAddress}", settings.IPAddress);
@@ -35,46 +35,116 @@ foreach (var arg in args)
     if (arg == "on")
     {
         await SendBraviaPowerCommand(true);
-        return;
+        return 0;
     }
     else if (arg == "off")
     {
         await SendBraviaPowerCommand(false);
-        return;
+        return 0;
+    }
+    else if (arg == "--service")
+    {
+        // Connect to D-Bus.
+        using var connection = new Connection(Address.Session!);
+        await connection.ConnectAsync();
+        logger.LogInformation("Connected to session bus.");
+
+        await connection.AddMatchAsync(
+            new MatchRule
+            {
+                Type = Tmds.DBus.Protocol.MessageType.Signal,
+                Sender = "org.gnome.ScreenSaver",
+                Path = "/org/gnome/ScreenSaver",
+                Member = "ActiveChanged",
+                Interface = "org.gnome.ScreenSaver"
+            },
+            (m, s) => m.GetBodyReader().ReadBool(),
+            (ex, arg, rs, hs) => ((Func<Exception?, bool, Task>)hs!).Invoke(ex, arg),
+            null,
+            // This argument is the 'hs' (handler state). Since it uses 'async/await', it returns a Task.
+            async (Exception? ex, bool active) => await OnActiveChanged(ex, active),
+            true,
+            ObserverFlags.None);
+
+        // Wait forever.
+        await Task.Delay(-1);
+
+        // We'll never get here
+        return 0;
     }
     else
     {
-        logger.LogInformation("Usage: gnome-bravia-screensaver [on|off|--help|-h]");
-        logger.LogInformation("A simple application that listens for the ActiveChanged signal from org.gnome.ScreenSaver and sends power on/off commands to a Bravia TV using Simple IP Control.");
+        logger.LogInformation("Usage: gnome-bravia-screensaver [on|off|--service|--help|-h]");
+        logger.LogInformation("A simple application that listens (--service) for the ActiveChanged signal from org.gnome.ScreenSaver and sends power on/off commands to a Bravia TV using Simple IP Control.");
+        logger.LogInformation("Launching the application without any parameters will open a GTK dialog allowing the user to manually control the TV.");
+        logger.LogInformation("The `on` or `off` parameters can be used to control the TV from the command line.");
         logger.LogInformation("Configuration is loaded from ~/.config/gnome-bravia-screensaver.config or gnome-bravia-screensaver.local.config (for local development).");
-        return;
+        return -1;
     }
 }
 
-// Connect to D-Bus.
-using var connection = new Connection(Address.Session!);
-await connection.ConnectAsync();
-logger.LogInformation("Connected to session bus.");
+// ------------------------------------------------------------
+// Launch the UI
+// ------------------------------------------------------------
 
-await connection.AddMatchAsync(
-    new MatchRule
+// Initialize a new GTK application
+var application = Application.New("org.lamothe.gnome-bravia-screensaver", Gio.ApplicationFlags.FlagsNone);
+
+// Declare the window outside the event so we can track its state
+ApplicationWindow? mainWindow = null;
+
+application.OnActivate += (sender, e) =>
+{
+    var app = (Application)sender;
+
+    // If the window already exists, a second instance tried to launch.
+    // Bring the existing window to the front and exit this event.
+    if (mainWindow != null)
     {
-        Type = MessageType.Signal,
-        Sender = "org.gnome.ScreenSaver",
-        Path = "/org/gnome/ScreenSaver",
-        Member = "ActiveChanged",
-        Interface = "org.gnome.ScreenSaver"
-    },
-    (m, s) => m.GetBodyReader().ReadBool(),
-    (ex, arg, rs, hs) => ((Func<Exception?, bool, Task>)hs!).Invoke(ex, arg), 
-    null,
-    // This argument is the 'hs' (handler state). Since it uses 'async/await', it returns a Task.
-    async (Exception? ex, bool active) => await OnActiveChanged(ex, active),
-    true,
-    ObserverFlags.None);
+        mainWindow.Present();
+        return;
+    }
 
-// Wait forever.
-await Task.Delay(-1);
+    // Otherwise, create the main window for the first time
+    mainWindow = ApplicationWindow.New(app);
+    mainWindow.Title = "GNOME Bravia Screensaver";
+    mainWindow.DefaultWidth = 240;
+    mainWindow.DefaultHeight = 100;
+    mainWindow.Resizable = false;
+
+    // Create a horizontal box layout with 15px spacing
+    var box = Box.New(Orientation.Horizontal, 15);
+    box.MarginTop = 20;
+    box.MarginBottom = 20;
+    box.MarginStart = 20;
+    box.MarginEnd = 20;
+    box.Halign = Align.Center;
+    box.Valign = Align.Center;
+
+    // "Turn On" Button (Native GNOME Blue)
+    var btnOn = Button.New();
+    btnOn.Label = "Turn On";
+    btnOn.WidthRequest = 90;
+    btnOn.AddCssClass("suggested-action");
+    btnOn.OnClicked += async (_, _) => await SendBraviaPowerCommand(true);
+
+    // "Turn Off" Button (Native GNOME Red)
+    var btnOff = Button.New();
+    btnOff.Label = "Turn Off";
+    btnOff.WidthRequest = 90;
+    btnOff.AddCssClass("destructive-action");
+    btnOff.OnClicked += async (_, _) => await SendBraviaPowerCommand(false);
+
+    // Add buttons to the box, and the box to the window
+    box.Append(btnOn);
+    box.Append(btnOff);
+    mainWindow.Child = box;
+
+    mainWindow.Show();
+};
+
+// Run the application loop
+return application.RunWithSynchronizationContext(null);
 
 async Task OnActiveChanged(Exception? ex, bool active)
 {
@@ -82,7 +152,16 @@ async Task OnActiveChanged(Exception? ex, bool active)
 
     try
     {
-        await SendBraviaPowerCommand(active);
+        // If screensaver is "active" then turn TV off.
+        var newTvState = !active;
+
+        // Only send the command if we're configured to.
+        var sendCommand = (newTvState && settings.AutoTurnOn) || (!newTvState && settings.AutoTurnOff);
+
+        if (sendCommand)
+        {
+            await SendBraviaPowerCommand(newTvState);
+        }
     }
     catch (Exception exception)
     {
@@ -92,7 +171,7 @@ async Task OnActiveChanged(Exception? ex, bool active)
 }
 
 async Task SendBraviaPowerCommand(bool powerOn)
-{    
+{
     if (powerOn)
     {
         await SendBraviaCommand(braviaPowerOnCommand, braviaSetInputHdmi1Command);
@@ -121,4 +200,6 @@ async Task SendBraviaCommand(params string[] commands)
 class BraviaSettings
 {
     public required string IPAddress { get; set; }
+    public bool AutoTurnOn { get; set; }
+    public bool AutoTurnOff { get; set; }
 }
